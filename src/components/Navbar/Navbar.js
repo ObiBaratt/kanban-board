66c13d34d82f27de47904b485151b462619d25b8
Obi Baratt <obibaratt@gmail.com>
2022-09-24 14:56:21 -0700

    testing CI/CD

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
         <nav className="navbar navbar-light bg-light">
             <div className='container'>
                 <span className="navbar-brand mb-0 h1"><img src="/kanban.svg" width="30" height="30" alt="Kanban Board" /></span>
-                <span className="navbar-brand"><h1><em>Kanbaneer</em></h1></span>
+                <span className="navbar-brand"><h1><em>Kanbaneer with CI/CD!</em></h1></span>
                 <span className="navbar-brand" onClick={()=> profileClickHandler()}>
                     <img src="/personIcon.svg" width="30" height="30" alt="Login Btn" />
                 </span>
