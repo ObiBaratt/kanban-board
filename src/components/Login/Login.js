d3e367357f24730481e1bc5c23fdbf4d380f1c07
Obi Baratt <obibaratt@gmail.com>
2022-09-24 17:49:20 -0700

    Update Login screen visuals, enable email/pw login/registration UI (non-functional).

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -46,53 +46,60 @@ export default function Login({ user, setUser }) {
         clearState();
     }
 
-    // if (!register) {
+    if (!register) {
         return (
             <>
             <Navbar />
             <div className="login-box container">
                 <div className="login">
-                    <h1>Login</h1>
-                    <button className="btn btn-dark" onClick={handleGoogleLogin}><img src="/google_logo.svg" alt="Google Logo" /></button>
-                    {/* <div className="spacer"> </div> */}
-                    {/* <h1>Email</h1> */}
-                    {/* <form onSubmit={handleEmailLogin}>
+                    <h1 className="login-header">Login</h1>
+                    <div className="spacer"> </div>
+                    <h3>Email Login</h3>
+                    <form onSubmit={handleEmailLogin}>
                         <div>
                             <input type="text" value={email} placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} ></input>
                             <br />
                             <input type="text" value={password} placeholder="Password (Will display)" onChange={(e) => {setPassword(e.target.value)}}></input>
                             <br />
-                            <input type="submit" value="Submit" />
+                            <input className="submit-btn" type="submit" value="Submit" />
                             <br />
                         </div>
-                    </form> */}
+                    </form>
+                    <div className="spacer"> </div>
+                    <h3>Or...</h3>
+                    <button className="login-btn" onClick={handleGoogleLogin}><img className="google-logo" src="/google_logo.svg" alt="Google Logo" /> Sign in with Google</button>
                 </div>
-                {/* <div className="toggle-register" onClick={() => {showRegister(!register)}}>New? Register here!</div> */}
+                <div className="toggle-register" onClick={() => {showRegister(!register)}}>New? Register here!</div>
             </div>
             </>
         )
-    // }
+    }
 
-//     else {
-//         return (
-//             <>
-//                 <h1 className="header">Your Private Kanban Board is a click away...</h1>
-//                 <div className="login-box container">
-//                     <div className="login">
-//                         <h1>Email</h1>
-//                         <form onSubmit={handleEmailRegister}>
-//                         <div>
-//                             <input type="text" value={email} placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} ></input>
-//                             <br />
-//                             <input type="text" value={password} placeholder="Password (Will display)" onChange={(e) => {setPassword(e.target.value)}}></input>
-//                             <br />
-//                             <input type="submit" value="Submit" />
-//                             <br />
-//                         </div>
-//                     </form>
-//                     </div>
-//                     <div className="toggle-register" onClick={() => {showRegister(!register)}}>Already have an account? Sign in instead!</div>
-//                 </div>
-//             </>
-//     )}
+    else {
+        return (
+            <>
+                <Navbar />
+                <div className="login-box container">
+                    <div className="login">
+                    <h1 className="login-header">Register</h1>
+                    <div className="spacer"> </div>
+                    <h3>Email Registration</h3>
+                        <form onSubmit={handleEmailRegister}>
+                        <div>
+                            <input type="text" value={email} placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} ></input>
+                            <br />
+                            <input type="text" value={password} placeholder="Password (Will display)" onChange={(e) => {setPassword(e.target.value)}}></input>
+                            <br />
+                            <input className="submit-btn" type="submit" value="Submit" />
+                            <br />
+                        </div>
+                    </form>
+                    <div className="spacer"> </div>
+                    <h3>Or...</h3>
+                    <button className="login-btn" onClick={handleGoogleLogin}><img className="google-logo" src="/google_logo.svg" alt="Google Logo" /> Sign in with Google</button>
+                    </div>
+                    <div className="toggle-register" onClick={() => {showRegister(!register)}}>Already have an account? Sign in instead!</div>
+                </div>
+            </>
+    )}
 };
