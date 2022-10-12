68d86a97ecfb28a6513a29c167356f20428d318f
Obi Baratt <obibaratt@gmail.com>
2022-10-12 16:09:22 -0700

    Removed unused code & variables for deploy. Moved WIP to addEmailLogin branch

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { registerWithEmail, loginWithEmail, loginWithGoogle } from "./loginHandler";
+import { useEffect } from "react";
+import { loginWithGoogle } from "./loginHandler";
 
 import "./Login.css";
 
@@ -7,9 +7,7 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
 export default function Login({ user, setUser }) {
-    const [register, showRegister] = useState(false);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -18,10 +16,6 @@ export default function Login({ user, setUser }) {
         }
     });
 
-    const clearState = () => {
-        setEmail("");
-        setPassword("");
-    }
 
     const handleGoogleLogin = async () => {
         const success = await loginWithGoogle();
@@ -29,24 +23,8 @@ export default function Login({ user, setUser }) {
         navigate("/home");
     }
 
-    const handleEmailLogin = (e) => {
-        e.preventDefault();
 
-        loginWithEmail(email, password)
 
-        clearState();
-    }
-
-    const handleEmailRegister = async (e) => {
-        e.preventDefault();
-
-        registerWithEmail(email, password).then(
-            loginWithEmail(email, password)
-        );
-        clearState();
-    }
-
-    // if (!register) {
         return (
             <>
             <Navbar />
@@ -54,45 +32,8 @@ export default function Login({ user, setUser }) {
                 <div className="login">
                     <h1>Login</h1>
                     <button className="btn btn-dark" onClick={handleGoogleLogin}><img src="/google_logo.svg" alt="Google Logo" /></button>
-                    {/* <div className="spacer"> </div> */}
-                    {/* <h1>Email</h1> */}
-                    {/* <form onSubmit={handleEmailLogin}>
-                        <div>
-                            <input type="text" value={email} placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} ></input>
-                            <br />
-                            <input type="text" value={password} placeholder="Password (Will display)" onChange={(e) => {setPassword(e.target.value)}}></input>
-                            <br />
-                            <input type="submit" value="Submit" />
-                            <br />
-                        </div>
-                    </form> */}
                 </div>
-                {/* <div className="toggle-register" onClick={() => {showRegister(!register)}}>New? Register here!</div> */}
             </div>
             </>
         )
-    // }
-
-//     else {
-//         return (
-//             <>
-//                 <h1 className="header">Your Private Kanban Board is a click away...</h1>
-//                 <div className="login-box container">
-//                     <div className="login">
-//                         <h1>Email</h1>
-//                         <form onSubmit={handleEmailRegister}>
-//                         <div>
-//                             <input type="text" value={email} placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} ></input>
-//                             <br />
-//                             <input type="text" value={password} placeholder="Password (Will display)" onChange={(e) => {setPassword(e.target.value)}}></input>
-//                             <br />
-//                             <input type="submit" value="Submit" />
-//                             <br />
-//                         </div>
-//                     </form>
-//                     </div>
-//                     <div className="toggle-register" onClick={() => {showRegister(!register)}}>Already have an account? Sign in instead!</div>
-//                 </div>
-//             </>
-//     )}
 };
